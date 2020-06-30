f51cf92e2138d2ceb6452f8ecbb4acb9bde4f272
Ross718 <rosario.rueda@corpogas.com.mx>
2020-06-30 13:13:09 -0500

    Se creó el menu principal con listview

--- app/src/main/java/com/szzcs/smartpos/Munu_Principal.java
+++ app/src/main/java/com/szzcs/smartpos/Munu_Principal.java
@@ -4,48 +4,77 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageButton;
-import android.widget.ImageView;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.szzcs.smartpos.Ticket.ventas;
-import com.zcs.sdk.card.CardReaderTypeEnum;
-
-import static com.zcs.sdk.card.CardReaderTypeEnum.MAG_CARD;
 
 public class Munu_Principal extends AppCompatActivity {
+    ListView list;
 
+    String[] maintitle ={
+            "Tickets","Facturas",
+            "Productos","Cortes",
+            "Pendientes","Reportes",
+    };
+
+    String[] subtitle ={
+            "Emite tickets de venta","Emisión de facturas",
+            "Administra productos","Realiza cortes de turnos",
+            "Enlista pendientes","Muestra reportes",
+    };
+
+    Integer[] imgid={
+            R.drawable.ventas,R.drawable.fact,
+            R.drawable.product,R.drawable.cortes,
+            R.drawable.pendientes, R.drawable.report,
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_munu__principal);
 
-        ImageView btnventa =  findViewById(R.id.iconVentas);
-        btnventa.setOnClickListener(new View.OnClickListener() {
+        MyListAdapter adapter=new MyListAdapter(this, maintitle, subtitle,imgid);
+        list= findViewById(R.id.list);
+        list.setAdapter(adapter);
+
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent( getApplicationContext(), ventas.class);
-                startActivity(intent);
+            public void onItemClick(AdapterView<?> parent, View view,int position, long id) {
+                // TODO Auto-generated method stub
+                if(position == 0) {
+                    //code specific to first list item
+                   // Toast.makeText(getApplicationContext(),"Place Your First Option Code",Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent( getApplicationContext(), ventas.class);
+                    startActivity(intent);
+                }
+
+                else if(position == 1) {
+                    //code specific to 2nd list item
+                    Toast.makeText(getApplicationContext(),"Place Your Second Option Code",Toast.LENGTH_SHORT).show();
+                }
+
+                else if(position == 2) {
+
+                    Toast.makeText(getApplicationContext(),"Place Your Third Option Code",Toast.LENGTH_SHORT).show();
+                }
+                else if(position == 3) {
+
+                    Toast.makeText(getApplicationContext(),"Place Your Forth Option Code",Toast.LENGTH_SHORT).show();
+                }
+                else if(position == 4) {
+
+                    Toast.makeText(getApplicationContext(),"Place Your Fifth Option Code",Toast.LENGTH_SHORT).show();
+                }
+                else if(position == 5) {
+
+                    Toast.makeText(getApplicationContext(),"Place Your Fifth Option Code",Toast.LENGTH_SHORT).show();
+                }
+
             }
         });
-//
-//        ImageButton btnPuntada = (ImageButton)findViewById(R.id.puntada);
-//        btnPuntada.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-////                Intent intent = new Intent(getApplicationContext(),leerTargeta.class);
-////                startActivity(intent);
-//                CardReaderTypeEnum cardType = MAG_CARD;
-//                CardFragment cf = new CardFragment();
-//                Bundle bundle = new Bundle();
-//                bundle.putSerializable("card_type", cardType);
-//                cf.setArguments(bundle);
-//                getFragmentManager().beginTransaction().replace(R.id.fragments_container, cf).
-//                        addToBackStack(CardFragment.class.getName()).
-//                        commit();
-//
-//                //cf.searchBankCard(cardType);
-//            }
-//        });
     }
-
 }
