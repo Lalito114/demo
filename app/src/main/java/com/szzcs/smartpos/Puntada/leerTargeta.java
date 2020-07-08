f685a9bcd2fd04acc94f7d50401a6fe9539becaa
Mikel0929 <miguel.reyes@corpogas.com.mx>
2020-07-08 09:47:22 -0500

    colores

--- app/src/main/java/com/szzcs/smartpos/Puntada/leerTargeta.java
+++ app/src/main/java/com/szzcs/smartpos/Puntada/leerTargeta.java
@@ -5,11 +5,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
 
+import com.szzcs.smartpos.MyListAdapter;
 import com.szzcs.smartpos.R;
 
 
 public class leerTargeta extends AppCompatActivity {
+    ListView list;
+
+    String[] maintitle ={
+            "Acumular","Redimir",
+            "Registrar",
+    };
+
+    String[] subtitle ={
+            "Acumula puntos","Redimir puntos",
+            "Registrar Tarjeta Puntada",
+    };
+
+    Integer[] imgid={
+            R.drawable.acumular,R.drawable.redimir,
+            R.drawable.registrar,
+    };
     Button btnRegistrar, btnAcumular;
 
     @Override
@@ -19,6 +37,9 @@ public class leerTargeta extends AppCompatActivity {
         Bundle bundle = getIntent().getExtras();
         final String track2 = bundle.getString("track");
 
+        MyListAdapter adapter=new MyListAdapter(this, maintitle, subtitle,imgid);
+        list= findViewById(R.id.list);
+        list.setAdapter(adapter);
 
         btnRegistrar = findViewById(R.id.btnRegistrar);
         btnRegistrar.setOnClickListener(new View.OnClickListener() {
