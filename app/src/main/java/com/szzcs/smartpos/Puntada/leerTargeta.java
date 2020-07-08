3f7dc76331043e5ec8424364de75ad21459e54d3
Mikel0929 <miguel.reyes@corpogas.com.mx>
2020-07-08 12:31:47 -0500

    colores

--- app/src/main/java/com/szzcs/smartpos/Puntada/leerTargeta.java
+++ app/src/main/java/com/szzcs/smartpos/Puntada/leerTargeta.java
@@ -4,11 +4,21 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import com.szzcs.smartpos.CardFragment;
 import com.szzcs.smartpos.MyListAdapter;
 import com.szzcs.smartpos.R;
+import com.szzcs.smartpos.Ticket.Entidad;
+import com.szzcs.smartpos.Ticket.ventas;
+import com.zcs.sdk.card.CardReaderTypeEnum;
+
+import java.util.List;
+
+import static com.zcs.sdk.card.CardReaderTypeEnum.MAG_CARD;
 
 
 public class leerTargeta extends AppCompatActivity {
@@ -28,6 +38,7 @@ public class leerTargeta extends AppCompatActivity {
             R.drawable.acumular,R.drawable.redimir,
             R.drawable.registrar,
     };
+
     Button btnRegistrar, btnAcumular;
 
     @Override
@@ -37,19 +48,47 @@ public class leerTargeta extends AppCompatActivity {
         Bundle bundle = getIntent().getExtras();
         final String track2 = bundle.getString("track");
 
-        MyListAdapter adapter=new MyListAdapter(this, maintitle, subtitle,imgid);
+        final MyListAdapter adapter=new MyListAdapter(this, maintitle, subtitle,imgid);
         list= findViewById(R.id.list);
         list.setAdapter(adapter);
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                switch (position) {
+                    case 0: //Acumular
+                        Intent intent = new Intent(getApplicationContext(), posicionCargaAcumular.class);
+                        Bundle bundle  = new Bundle();
+                        bundle.putString("track",track2);
+                        intent.putExtras(bundle);
+                        startActivity(intent);
+                    break;
+                    case 1://Redimir
+                        Toast.makeText(getApplicationContext(),"En construcción",Toast.LENGTH_SHORT).show();
+                    break;
+                    case 2://Registrar
+                        Intent intent1 = new Intent(getApplicationContext(), ClavePuntada.class);
+                        Bundle bundle1  = new Bundle();
+                        bundle1.putString("track",track2);
+                        intent1.putExtras(bundle1);
+                        startActivity(intent1);
+                    break;
+                    default:
+
+                    break;
+                }
+            }
+        });
 
         btnRegistrar = findViewById(R.id.btnRegistrar);
         btnRegistrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), ClavePuntada.class);
-                Bundle bundle  = new Bundle();
-                bundle.putString("track",track2);
-                intent.putExtras(bundle);
-                startActivity(intent);
+                //Intent intent = new Intent(getApplicationContext(), ClavePuntada.class);
+                //Bundle bundle  = new Bundle();
+                //bundle.putString("track",track2);
+                //intent.putExtras(bundle);
+                //startActivity(intent);
             }
         });
 
@@ -57,11 +96,11 @@ public class leerTargeta extends AppCompatActivity {
         btnAcumular.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), posicionCargaAcumular.class);
-                Bundle bundle  = new Bundle();
-                bundle.putString("track",track2);
-                intent.putExtras(bundle);
-                startActivity(intent);
+                //Intent intent = new Intent(getApplicationContext(), posicionCargaAcumular.class);
+                //Bundle bundle  = new Bundle();
+                //bundle.putString("track",track2);
+                //intent.putExtras(bundle);
+                //startActivity(intent);
             }
         });
     }
