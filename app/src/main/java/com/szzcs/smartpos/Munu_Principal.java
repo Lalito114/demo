2c285c33c94272b0857bc1c1ef443a387bd65248
Mikel0929 <miguel.reyes@corpogas.com.mx>
2020-07-24 12:27:17 -0500

    clave usuario

--- app/src/main/java/com/szzcs/smartpos/Munu_Principal.java
+++ app/src/main/java/com/szzcs/smartpos/Munu_Principal.java
@@ -8,7 +8,7 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
 
-import com.szzcs.smartpos.Productos.VentaProductos;
+import com.szzcs.smartpos.Productos.posicionProductos;
 import com.szzcs.smartpos.Ticket.ventas;
 import com.zcs.sdk.card.CardReaderTypeEnum;
 
@@ -85,7 +85,7 @@ public class Munu_Principal extends AppCompatActivity {
 
                 else if(position == 2) {
 
-                    Intent intent = new Intent(getApplicationContext(), VentaProductos.class);
+                    Intent intent = new Intent(getApplicationContext(), posicionProductos.class);
                     startActivity(intent);
                 }
                 else if(position == 3) {
