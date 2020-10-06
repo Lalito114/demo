d350302d9c2d2c55098e7b26990ec25a84a530b3
IranCarrillo <51378756+IranCarrillo02@users.noreply.github.com>
2020-10-06 11:22:58 -0500

    Finalicé subtotal oficina

--- app/src/main/java/com/szzcs/smartpos/Cortes2/SubOfiBilletes.java
+++ app/src/main/java/com/szzcs/smartpos/Cortes2/SubOfiBilletes.java
@@ -76,7 +76,6 @@ public class SubOfiBilletes extends AppCompatActivity {
 
                             enviarFolios(maintitle.get(i), String.valueOf(arrayMonto.get(i)));
 
-
                         }
 
 
