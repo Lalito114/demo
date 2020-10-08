8ca74fea28ba51df7b3ce2dd1f580c543de0fd59
Lalito114 <eduardo.rivera@corpogas.com.mx>
2020-10-08 09:36:04 -0500

    Segunda parte de configuracion

--- app/src/main/java/com/szzcs/smartpos/Puntada/Redimir/ListAdapterProductosRedimir.java
+++ app/src/main/java/com/szzcs/smartpos/Puntada/Redimir/ListAdapterProductosRedimir.java
@@ -6,35 +6,44 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.szzcs.smartpos.Productos.VentasProductos;
 import com.szzcs.smartpos.R;
+import com.szzcs.smartpos.TanqueLleno.ProductoTLl;
 
 import java.util.List;
 
 public class ListAdapterProductosRedimir extends ArrayAdapter<String> {
     private final Activity context;
-    private final String[] ID;
-    private final String[] DescripcionProducto;
+    private final String[] maintitle;
+    private final String[] subtitle;
+    private final Integer[] imgid;
 
-    public ListAdapterProductosRedimir(BalanceProductos context, List<String> ID, List<String> DescripcionProducto){
-        super((Context) context, R.layout.list, ID);
+    public ListAdapterProductosRedimir(BalanceProductos context, List<String> maintitle, List<String> subtitle, List<Integer> imgid) {
+        super((Context) context, R.layout.list, maintitle);
+        // TODO Auto-generated constructor stub
 
-        this.context=(Activity) context;
-        this.ID = ID.toArray(new String[0]);
-        this.DescripcionProducto = DescripcionProducto.toArray(new String[0]);
+        this.context= (Activity) context;
+        this.maintitle= maintitle.toArray(new String[0]);
+        this.subtitle= subtitle.toArray(new String[0]);
+        this.imgid= imgid.toArray(new Integer[0]);
+
     }
+
 
     public View getView(int position, View view, ViewGroup parent) {
         LayoutInflater inflater=context.getLayoutInflater();
-        View rowView=inflater.inflate(R.layout.listaproductos, null,true);
+        View rowView=inflater.inflate(R.layout.mylist, null,true);
 
         TextView titleText = (TextView) rowView.findViewById(R.id.title);
+        ImageView imageView = (ImageView) rowView.findViewById(R.id.icon);
         TextView subtitleText = (TextView) rowView.findViewById(R.id.subtitle);
 
-        titleText.setText(ID[position]);
-        subtitleText.setText(DescripcionProducto[position]);
+        titleText.setText(maintitle[position]);
+        imageView.setImageResource(imgid[position]);
+        subtitleText.setText(subtitle[position]);
 
         return rowView;
 
