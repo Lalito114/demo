d302bec78e0888163e96673cbe8bec49cc4276f0
Mikel <miguel.reyes@corpogas.com.mx>
2020-10-30 15:57:20 -0600

    posicionFinaliza

--- app/src/main/java/com/szzcs/smartpos/FinalizaVenta/posicionFinaliza.java
+++ app/src/main/java/com/szzcs/smartpos/FinalizaVenta/posicionFinaliza.java
@@ -93,9 +93,9 @@ public class posicionFinaliza extends AppCompatActivity {
 
                             if (ObjetoRespuesta.equals("null")){
                                 //Toast.makeText(posicionProductos.this, mensaje, Toast.LENGTH_SHORT).show();
-                                try {
+                                //try {
                                     AlertDialog.Builder builder = new AlertDialog.Builder(posicionFinaliza.this);
-                                    builder.setTitle("Productos");
+                                    builder.setTitle("Finaliza Venta");
                                     builder.setCancelable(false);
                                     builder.setMessage("" + mensaje)
                                             .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
@@ -106,9 +106,9 @@ public class posicionFinaliza extends AppCompatActivity {
                                                     finish();
                                                 }
                                             }).show();
-                                }catch (Exception e){
-                                    e.printStackTrace();
-                                }
+                                //}catch (Exception e){
+                                //    e.printStackTrace();
+                                //}
 
                             }else {
                                 JSONObject jsonObject1 = new JSONObject(ObjetoRespuesta);
@@ -154,37 +154,37 @@ public class posicionFinaliza extends AppCompatActivity {
                                     }
 
                                 }
+                                if (banderaposicionCarga.equals(false)){
+                                    Toast.makeText(posicionFinaliza.this, "No hay Posiciones de Carga para Finalizar Venta", Toast.LENGTH_SHORT).show();
+                                    Intent intent1 = new Intent(getApplicationContext(), Munu_Principal.class);
+                                    startActivity(intent1);
+                                    finish();
+                                }else {
+                                    ListAdapterProd adapterProd = new ListAdapterProd(posicionFinaliza.this, maintitle, subtitle, imgid);
+                                    list = (ListView) findViewById(R.id.list);
+                                    list.setAdapter(adapterProd);
+
+                                    list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+                                        @Override
+                                        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                                            // TODO Auto-generated method stub
+                                            String numerocarga = maintitle1.get(position);
+                                            posicion = numerocarga;
+                                            //Se llama la clase para la clave del usuario
+                                            if (lugarproviene.equals("1")) {
+                                                solicitadespacho();
+                                            } else {
+                                                validaPosicionDisponible(numerocarga);
+                                            }
+
+
+                                        }
+                                    });
+                                }
                             }
                         } catch (JSONException e) {
                             e.printStackTrace();
-                        }
-                        if (banderaposicionCarga.equals(false)){
-                            Toast.makeText(posicionFinaliza.this, "No hay Posiciones de Carga para Finalizar Venta", Toast.LENGTH_SHORT).show();
-                            Intent intent1 = new Intent(getApplicationContext(), Munu_Principal.class);
-                            startActivity(intent1);
-                            finish();
-                        }else {
-                            ListAdapterProd adapterProd = new ListAdapterProd(posicionFinaliza.this, maintitle, subtitle, imgid);
-                            list = (ListView) findViewById(R.id.list);
-                            list.setAdapter(adapterProd);
-
-                            list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-                                @Override
-                                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                                    // TODO Auto-generated method stub
-                                    String numerocarga = maintitle1.get(position);
-                                    posicion = numerocarga;
-                                    //Se llama la clase para la clave del usuario
-                                    if (lugarproviene.equals("1")) {
-                                        solicitadespacho();
-                                    } else {
-                                        validaPosicionDisponible(numerocarga);
-                                    }
-
-
-                                }
-                            });
                         }
                     }
                     //funcion para capturar errores
